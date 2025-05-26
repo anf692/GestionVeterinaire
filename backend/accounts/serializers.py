54320b9c287ed1babbb7a73a966b26c1761e8bff
ksene21 <khadysene21122001@gmail.com>
2025-05-26 12:59:29 +0100

    Ajout du model et serializers de l'app account

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -5,3 +5,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'email', 'first_name', 'last_name', 'role']
+
+
+
+
+
