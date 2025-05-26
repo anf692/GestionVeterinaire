65655f196f106a945ae9770ef7788613ef9a477d
[die <diediouf@gmail.com>
2025-05-26 09:00:47 -0300

    Creation du models owner

--- backend/owners/models.py
+++ backend/owners/models.py
@@ -1,2 +1,15 @@
 from django.db import models
 
+class Owner(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.TextField()
+    phone = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+ 
+
