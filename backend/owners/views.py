819b66587f62f3559d1b75a5631ab5e14e46ad62
alytimera <alytimera7@gmail.com>
2025-05-26 19:21:03 +0000

    Message clair sur les modifications

--- backend/owners/views.py
+++ backend/owners/views.py
@@ -1,1 +1,10 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Owner
+from .serializers import OwnerSerializer
 
+class OwnerViewSet(viewsets.ModelViewSet):
+    queryset = Owner.objects.all()
+    serializer_class = OwnerSerializer
+
+# Create your views here.
