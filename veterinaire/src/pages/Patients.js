1458ccde02f8b63ff465047b4b3510d1a39149d4
[die <diediouf@gmail.com>
2025-05-30 08:46:33 -0300

    creation patient

---
+++ veterinaire/src/pages/Patients.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PatientList from '../components/patients/PatientList';
+
+const Patients = () => {
+    return (
+        <div>
+            <h1>Gestion des patients</h1>
+            <PatientList />
+        </div>
+    );
+};
+
+export default Patients;
