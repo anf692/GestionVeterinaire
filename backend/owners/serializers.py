5f863aab9258b5029f2f491fe27424d16c6a17a9
anf692 <an.fall5@isepat.edu.sn>
2025-06-09 00:51:28 +0100

    Remove pyc files from repository

--- backend/owners/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Owner
-
-class OwnerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Owner
-        fields = '__all__'
